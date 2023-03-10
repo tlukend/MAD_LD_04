f42bde74171337dac66589aed91246dee19fd574
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2023-03-10 16:54:04 +0100

    startet with navigation

---
+++ app/src/main/java/com/example/movieappmad23/navigation/Navigation.kt
@@ -0,0 +1,17 @@
+package com.example.movieappmad23.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.movieappmad23.screens.Screen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route){
+
+        }
+    }
+}
